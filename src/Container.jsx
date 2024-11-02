0ba4dd49b15c4a61b3a92ff2cdf7398706920668
Roman <polskaago@gmail.com>
2024-11-01 22:35:37 -0400

    Prop modification and CSS styling

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,20 +4,18 @@ import TextArea from './TextArea';
 
 export default function Container() {
     const [text, setText] = useState('');
-    const numberOfWords = text.split(/\s/).filter((word) => word !== '').length;
-    const numberOfCharacters = text.length;
-    const instagramCharactersLeft = 280 - text.length;
-    const facebookCharactersLeft = 2200 - text.length;
+
+    const stats = {
+        numberOfWords: text.split(/\s/).filter((word) => word !== '').length,
+        numberOfCharacters: text.length,
+        instagramCharactersLeft: 280 - text.length,
+        facebookCharactersLeft: 2200 - text.length,
+    };
 
     return (
         <main className='container'>
             <TextArea text={text} setText={setText} />
-            <Stats
-                numberOfCharacters={numberOfCharacters}
-                instagramCharactersLeft={instagramCharactersLeft}
-                facebookCharactersLeft={facebookCharactersLeft}
-                numberOfWords={numberOfWords}
-            />
+            <Stats stats={stats} />
         </main>
     );
 }
--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,15 +1,10 @@
-export default function Stats({
-    numberOfCharacters,
-    instagramCharactersLeft,
-    facebookCharactersLeft,
-    numberOfWords,
-}) {
+export default function Stats({ stats }) {
     return (
         <section className='stats'>
-            <Stat number={numberOfWords} label='Words' />
-            <Stat number={numberOfCharacters} label='Characters' />
-            <Stat number={instagramCharactersLeft} label='Instagram' />
-            <Stat number={facebookCharactersLeft} label='Facebook' />
+            <Stat number={stats.numberOfWords} label='Words' />
+            <Stat number={stats.numberOfCharacters} label='Characters' />
+            <Stat number={stats.instagramCharactersLeft} label='Instagram' />
+            <Stat number={stats.facebookCharactersLeft} label='Facebook' />
         </section>
     );
 }
@@ -17,7 +12,13 @@ export default function Stats({
 function Stat({ number, label }) {
     return (
         <section className='stat'>
-            <span className='stat_number'>{number}</span>
+            <span
+                className={`stat__number ${
+                    number < 0 ? 'stat__number--limit' : ''
+                }`}
+            >
+                {number}
+            </span>
             <h2 className='second-heading'>{label}</h2>
         </section>
     );
